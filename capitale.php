47dc735ef652318cf75a9ff3cab936cd98649faa
Courmont <gael.courmont@isen.yncrea.fr>
2020-10-22 15:34:33 +0200

    phpv1 first pages

---
+++ capitale.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Geoquizz</title>
+        <link rel="stylesheet" href="main.css" />
+        <link rel="stylesheet" href="common.css" />
+    </head>
+    <?php include 'header.php'?>
+    <body>
+            <div class="header">
+                <h1 id="title">Quiz #1</h1>
+            </div>
+
+            <div>
+                <div class="quesFrame" >'
+                    <p style="font-size: 22;">click on the spain capital</p>
+                    <form action="submitAction.php"  method = "POST">
+                        <input type="radio" name="value" value="male"> paris<br>
+                        <input type="radio" name="value" value="female"> londres<br>
+                        <input type="radio" name="value" value="other"> madrid
+                   
+                </div>
+
+                <div class="quesFrame">
+                    <p style="font-size: 22;">click on the french capital</p>
+                    
+                        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                        <label for="vehicle1"> paris</label><br>
+                        <input type="checkbox" id="vehicle2" name="vehicle2" value="Car">
+                        <label for="vehicle2"> londres</label><br>
+                        <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat">
+                        <label for="vehicle3"> barcelone</label><br>
+                    
+                </div>
+            
+                
+                        <input type="Submit" value="Submit" class="submitBtn">
+
+                    </form>
+
+
+
+      
+            </div>
+
+        </body>
+    <?php include 'Footer.php'?>
+</html>
---
+++ header.php
@@ -0,0 +1,20 @@
+<header>
+    <a id="headertitle" href="home.php">Geo quizz</a>
+    <nav>
+    <div class="menuCategory">
+        <a href="search.php">Contact<a/>
+    </div>
+
+    <div class="menuCategory">
+        <span class="navtitle">Quizz</span>
+        <div class="withSeveralLinks">
+            <a href="capitale.php">
+                capitale
+            </a>
+            <a href="musique.php">
+                musique
+            </a>
+        </div>
+    </div>
+    </nav>
+</header>
