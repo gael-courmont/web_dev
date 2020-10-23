a951a49b67f6aedc1aa1531c6a59b71feee41127
Courmont <gael.courmont@isen.yncrea.fr>
2020-10-23 20:06:27 +0200

    phpv1_final

---
+++ database/quizzdb.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 23, 2020 at 03:37 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quizzdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `answer`
+--
+
+DROP TABLE IF EXISTS `answer`;
+CREATE TABLE IF NOT EXISTS `answer` (
+  `answer_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'answer identifier',
+  `answer_text` varchar(255) NOT NULL COMMENT 'text of the answer',
+  `is_valid_answer` tinyint(1) NOT NULL COMMENT 'valid answer for question',
+  `answer_question_id` int(11) NOT NULL COMMENT 'question related',
+  PRIMARY KEY (`answer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `answer`
+--
+
+INSERT INTO `answer` (`answer_id`, `answer_text`, `is_valid_answer`, `answer_question_id`) VALUES
+(1, 'paris', 0, 1),
+(2, 'london', 0, 1),
+(3, 'madrid', 1, 1),
+(5, 'madrid', 0, 2),
+(6, 'london', 0, 2),
+(7, 'paris', 1, 2),
+(8, 'paris', 0, 3),
+(9, 'london', 1, 3),
+(10, 'madrid', 0, 3),
+(11, 'human after all', 0, 4),
+(12, 'alive', 0, 4),
+(13, 'random access memories', 1, 4),
+(14, 'angus young', 1, 5),
+(15, 'jimmy page', 0, 5),
+(16, 'eddie van halen', 0, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+CREATE TABLE IF NOT EXISTS `question` (
+  `question_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'question_identification',
+  `question_title` varchar(255) NOT NULL COMMENT 'title of the question',
+  `question_quizz_id` int(11) NOT NULL COMMENT 'link question quizz',
+  `question_input_type` varchar(255) NOT NULL COMMENT 'input of the question',
+  PRIMARY KEY (`question_id`),
+  KEY `question_quizz_id_fk` (`question_quizz_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `question`
+--
+
+INSERT INTO `question` (`question_id`, `question_title`, `question_quizz_id`, `question_input_type`) VALUES
+(1, 'what is the capital city of spain', 1, 'checkbox'),
+(2, 'what is the capital city of france', 1, 'checkbox'),
+(3, 'what is the capital city of england', 1, 'checkbox'),
+(4, 'what is the name of the daft punk last album', 2, 'radio'),
+(5, 'type the name of acdc guitarist', 2, 'string');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizz`
+--
+
+DROP TABLE IF EXISTS `quizz`;
+CREATE TABLE IF NOT EXISTS `quizz` (
+  `quizz_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Quizz Identifiant',
+  `quizz_name` varchar(255) NOT NULL COMMENT 'Quizz name',
+  PRIMARY KEY (`quizz_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `quizz`
+--
+
+INSERT INTO `quizz` (`quizz_id`, `quizz_name`) VALUES
+(1, 'capitale'),
+(2, 'musique');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'user identifiant',
+  `user_last_name` varchar(255) NOT NULL COMMENT 'user last name',
+  `user_first_name` varchar(255) NOT NULL COMMENT 'user first name',
+  `user_adress` longtext COMMENT 'user physical adress',
+  `user_phone` varchar(255) DEFAULT NULL COMMENT 'user phone',
+  `user_birthdate` datetime DEFAULT NULL,
+  `user_password` varchar(255) NOT NULL COMMENT 'User Password',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_last_name`, `user_first_name`, `user_adress`, `user_phone`, `user_birthdate`, `user_password`) VALUES
+(1, 'courmont', 'gael', 'rue de douai', '0781', NULL, 'les3freres'),
+(10, 'gael', 'couront', NULL, NULL, NULL, 'les3freres'),
+(11, 'gael', 'Courmont', NULL, NULL, NULL, 'les3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_answer`
+--
+
+DROP TABLE IF EXISTS `user_answer`;
+CREATE TABLE IF NOT EXISTS `user_answer` (
+  `user_answer_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'User answer identifiant',
+  `user_id` int(11) NOT NULL COMMENT 'user identifiant',
+  `answer_id` int(11) NOT NULL COMMENT 'answer_id',
+  `user_answer_date` timestamp NULL DEFAULT NULL COMMENT 'date of answer user',
+  PRIMARY KEY (`user_answer_id`),
+  KEY `user_id_fk` (`user_id`),
+  KEY `answer_id_fk` (`answer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_answer`
+--
+
+INSERT INTO `user_answer` (`user_answer_id`, `user_id`, `answer_id`, `user_answer_date`) VALUES
+(1, 1, 1, '2020-10-23 08:50:09');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `question`
+--
+ALTER TABLE `question`
+  ADD CONSTRAINT `question_quizz_id_fk` FOREIGN KEY (`question_quizz_id`) REFERENCES `quizz` (`quizz_id`);
+
+--
+-- Constraints for table `user_answer`
+--
+ALTER TABLE `user_answer`
+  ADD CONSTRAINT `answer_id_fk` FOREIGN KEY (`answer_id`) REFERENCES `answer` (`answer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
